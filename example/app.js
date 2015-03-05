197c59cfa35357a59a9252056bedeea90324a125
MaiTar <maialee@Maias-MacBook-Pro-2.local>
2015-03-04 21:28:15 -0800

    added new functionality to allow user to add optional on/off text

--- example/app.js
+++ example/app.js
@@ -1,7 +1,10 @@
 angular.module('app', ['uiSwitch'])
 
 .controller('MyController', function($scope) {
-  $scope.enabled = true;
+   $scope.enabled = true;
+  $scope.onOff = true;
+  $scope.yesNo = true;
+
 
   $scope.changeCallback = function() {
     console.log('This is the state of my model ' + $scope.enabled);
