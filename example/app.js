71a40f97afaa926a21127a5adf8e76d817812ad7
Raphael Müller <raaphii@gmail.com>
2015-11-17 09:16:28 +0100

    Added example with dynamic update of disabled property Rebuild of minified css and js

--- example/app.js
+++ example/app.js
@@ -1,11 +1,15 @@
 angular.module('app', ['uiSwitch'])
 
-.controller('MyController', function($scope) {
+.controller('MyController', function($scope, $timeout) {
   $scope.enabled = true;
   $scope.onOff = true;
   $scope.yesNo = true;
   $scope.disabled = true;
-
+	
+	$scope.dynamicDisabled = true;
+	$timeout(function() {
+		$scope.dynamicDisabled = false;
+	}, 1000);
 
   $scope.changeCallback = function() {
     console.log('This is the state of my model ' + $scope.enabled);
