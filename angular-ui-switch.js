c025ab027976c1b50fa3139f40c4a2685cf9160d
Matthew Wilson <wormsetc@gmail.com>
2016-05-12 11:25:15 -0400

    Remove ternary conditionals from template
    Ensure attributes are defined

--- angular-ui-switch.js
+++ angular-ui-switch.js
@@ -7,20 +7,28 @@ angular.module('uiSwitch', [])
   , transclude: true
   , template: function(element, attrs) {
       var html = '';
+      attrs.class +=  '';
+      attrs.ngModel += '';
+      attrs.name += '';
+      attrs.id += '';
+      attrs.disabled += '';
+      attrs.on += '';
+      attrs.off += '';
+      
       html += '<span';
-      html +=   ' class="switch' + (attrs.class ? ' ' + attrs.class : '') + '"';
+      html +=   ' class="switch ' + attrs.class + '"';
       html +=   attrs.ngModel ? ' ng-click="' + attrs.disabled + ' ? ' + attrs.ngModel + ' : ' + attrs.ngModel + '=!' + attrs.ngModel + (attrs.ngChange ? '; ' + attrs.ngChange + '()"' : '"') : '';
       html +=   ' ng-class="{ checked:' + attrs.ngModel + ', disabled:' + attrs.disabled + ' }"';
       html +=   '>';
       html +=   '<small></small>';
       html +=   '<input type="checkbox"';
-      html +=     attrs.id ? ' id="' + attrs.id + '"' : '';
-      html +=     attrs.name ? ' name="' + attrs.name + '"' : '';
-      html +=     attrs.ngModel ? ' ng-model="' + attrs.ngModel + '"' : '';
+      html +=     ' id="' + attrs.id + '"';
+      html +=     ' name="' + attrs.name + '"';
+      html +=     ' ng-model="' + attrs.ngModel + '"';
       html +=     ' style="display:none" />';
       html +=     '<span class="switch-text">'; /*adding new container for switch text*/
-      html +=     attrs.on ? '<span class="on">'+attrs.on+'</span>' : ''; /*switch text on value set by user in directive html markup*/
-      html +=     attrs.off ? '<span class="off">'+attrs.off + '</span>' : ' ';  /*switch text off value set by user in directive html markup*/
+      html +=     '<span class="on">'+attrs.on+'</span>'; /*switch text on value set by user in directive html markup*/
+      html +=     '<span class="off">'+attrs.off + '</span>';  /*switch text off value set by user in directive html markup*/
       html += '</span>';
       return html;
     }
