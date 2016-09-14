453e97008cafa2757ee1e7111b0d1711b6fe71a2
lihs <lhsai9@vip.qq.com>
2016-09-15 04:56:35 +0800

    1.using ng-disabled not disable and disabled is only disabled on html;2.fix bug that make ng-change work like ng-click. issue #50;

--- angular-ui-switch.js
+++ angular-ui-switch.js
@@ -7,21 +7,26 @@ angular.module('uiSwitch', [])
   , transclude: true
   , template: function(element, attrs) {
       var html = '';
+      html += '<label>';
       html += '<span';
       html +=   ' class="switch' + (attrs.class ? ' ' + attrs.class : '') + '"';
-      html +=   attrs.ngModel ? ' ng-click="' + attrs.disabled + ' ? ' + attrs.ngModel + ' : ' + attrs.ngModel + '=!' + attrs.ngModel + (attrs.ngChange ? '; ' + attrs.ngChange + '()"' : '"') : '';
-      html +=   ' ng-class="{ checked:' + attrs.ngModel + ', disabled:' + attrs.disabled + ' }"';
+      html +=   attrs.ngModel ? ' ng-click="' + attrs.ngDisabled + ' || ' + attrs.disabled ? true : false + ' ? ' + attrs.ngModel + ' : ' + attrs.ngModel + '=!' + attrs.ngModel + '"' : '';
+      html +=   ' ng-class="{ checked:' + attrs.ngModel + ', disabled:' + attrs.ngDisabled + ' }"';
       html +=   '>';
       html +=   '<small></small>';
       html +=   '<input type="checkbox"';
       html +=     attrs.id ? ' id="' + attrs.id + '"' : '';
       html +=     attrs.name ? ' name="' + attrs.name + '"' : '';
       html +=     attrs.ngModel ? ' ng-model="' + attrs.ngModel + '"' : '';
+      html +=     attrs.ngChange ? ' ng-change="' + attrs.ngChange + '"' : '';
+      html +=     attrs.ngDisabled ? ' ng-disabled="' + attrs.ngDisabled + '"' : '';
+      html +=     attrs.disabled ? ' disabled="' + attrs.disabled + '"' : '';
       html +=     ' style="display:none" />';
       html +=     '<span class="switch-text">'; /*adding new container for switch text*/
       html +=     attrs.on ? '<span class="on">'+attrs.on+'</span>' : ''; /*switch text on value set by user in directive html markup*/
       html +=     attrs.off ? '<span class="off">'+attrs.off + '</span>' : ' ';  /*switch text off value set by user in directive html markup*/
       html += '</span>';
+      html += '</label>';
       return html;
     }
   }
