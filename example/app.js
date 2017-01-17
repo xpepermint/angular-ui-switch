e2f100a8eea7262e61ee0cbfb82e481083993858
tyrion85 <tyrion85@gmail.com>
2017-01-17 20:26:39 +0100

    Removed explicit setting of brackets for ngChange attribute. Code should be backward compatibile - for settings without brackets, they should still be inserted automatically. This solves the issue #10

--- example/app.js
+++ example/app.js
@@ -2,6 +2,7 @@ angular.module('app', ['uiSwitch'])
 
 .controller('MyController', function($scope) {
   $scope.enabled = true;
+  $scope.enabledWithArguments = true;
   $scope.onOff = true;
   $scope.yesNo = true;
   $scope.disabled = true;
@@ -9,5 +10,9 @@ angular.module('app', ['uiSwitch'])
 
   $scope.changeCallback = function() {
     console.log('This is the state of my model ' + $scope.enabled);
+  };
+  
+  $scope.changeCallbackWithArguments = function(arg) {
+    console.log('This is the state of my model ' + $scope.enabledWithArguments + '\nValue of passed argument is "' + arg.someArgument + '"' + "\n");
   };
 });
